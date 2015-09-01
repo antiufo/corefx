71b63b492163ebf6f33b5a860326082e185f641a
Andrea Martinelli <martinelli-andrea@outlook.com>
2015-09-01 15:20:37 +0200

    .net project for interpreter

---
+++ src/System.Linq.Expressions/src/SR-Common.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    internal static partial class SR
+    {
+        public static string GetResourceString(string str, object unk)
+        {
+            return str;
+        }
+        public static string Format(string str, params object[] args)
+        {
+            return str;
+        }
+    }
+}
